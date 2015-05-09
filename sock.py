f1bb08aef8dbeee7593c3d9868d89baf705f9e92
Ashish Dubey <ashish.dubey91@gmail.com>
2015-05-09 19:49:46 +0530

    sort of working!

--- sock.py
+++ sock.py
@@ -6,16 +6,16 @@ from gevent import Greenlet, sleep
 
 
 class ThrottledSocket(object):
-    def __init__(self, socket, limit):
+    def __init__(self, bind_socket, limit):
         self.limit = limit
         self.current = 0
-        self.write_queue = []
-        self.socket = socket
+        self.write_queue = ''
+        self.socket = bind_socket
         self.write_loop = None
         self.rate_loop = None
 
-    def _socket_writeable(self):
-        select.select([], [self.socket], [])
+    def _socket_writable(self):
+        select.select([], [self.socket.fileno()], [])
         return True
 
     def _write_loop(self):
@@ -23,14 +23,17 @@ class ThrottledSocket(object):
             if self.current < self.limit:
                 available_n = self.limit - self.current
             else:
+                sleep(0)
                 continue
 
-            if self.socket_writable():
-                bytes = self.queue.pop(available_n)
-                socket.write(bytes)
+            if self._socket_writable():
+                bytes = self.write_queue[:available_n]
+                self.write_queue = self.write_queue[available_n:]
+                self.socket.send(bytes)
 
                 # TODO: Handle -> maybe the socket does not write all available_n.
                 self._update_rate(available_n)
+            sleep(0)
 
     def _rate_loop(self):
         while True:
@@ -40,14 +43,14 @@ class ThrottledSocket(object):
     def _update_rate(self, n):
         self.current += n
 
-        if self.rate_loop:
-            self.rate_loop = Greenlet.spawn(functools.partial(self.rate_loop, self))
+        if not self.rate_loop:
+            self.rate_loop = Greenlet.spawn(self._rate_loop)
 
     def write(self, bytes):
         if not self.write_loop:
-            self.write_loop = Greenlet.spawn(functools.partial(self.write_loop, self))
+            self.write_loop = Greenlet.spawn(self._write_loop)
 
-        self.write_queue.push(bytes)
+        self.write_queue += bytes
 
     def read(self, nbytes=0):
         if self.current < self.limit:
